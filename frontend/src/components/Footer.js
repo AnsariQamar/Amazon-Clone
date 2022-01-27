c50971774fdec13d5767a44c9493a538d8012420
AnsariQamar <ansariqamar76@gmail.com>
2022-01-27 13:11:24 +0530

    firstcommit

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            Ansari Qamar © 2022 All Rights Reserved Amazon Clone
+        </div>
+    )
+}
